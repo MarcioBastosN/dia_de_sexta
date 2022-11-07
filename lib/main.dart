0721d8307ae364a966f3b253a9efac5f9ca22afb
MarcioBastos <marciobastosn@gmail.com>
2022-11-07 12:16:39 -0300

    add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:dia_de_sexta/model/jogo.dart';
+import 'package:dia_de_sexta/provider/providerJogo.dart';
 import 'package:dia_de_sexta/view/home.dart';
 import 'package:dia_de_sexta/view/placar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,16 +14,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Dia de Sexta',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        // ChangeNotifierProvider(
+        //   create: (context) => ProviderJogo(),
+        // ),
+        // ChangeNotifierProvider(
+        //   create: (context) => Jogo(),
+        // ),
+      ],
+      child: MaterialApp(
+        title: 'Dia de Sexta',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        routes: {
+          '/': (context) => Home(),
+          'placar': (context) => const Placar(title: 'Dia de Sexta'),
+        },
+        // home: const Placar(title: 'Dia de Sexta'),
       ),
-      routes: {
-        '/': (context) => Home(),
-        'placar': (context) => const Placar(title: 'Dia de Sexta'),
-      },
-      // home: const Placar(title: 'Dia de Sexta'),
     );
   }
 }
