3df4c77ce3a648efa4ea634b990738ac3995c070
MarcioBastos <marciobastosn@gmail.com>
2022-11-08 10:43:11 -0300

    ajustes

--- lib/model/jogo.dart
+++ lib/model/jogo.dart
@@ -1,4 +1,6 @@
+import 'package:dia_de_sexta/provider/providerJogo.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Jogo with ChangeNotifier {
   String? equipe_1;
@@ -6,6 +8,11 @@ class Jogo with ChangeNotifier {
   late int pontosEquipe_1 = 0;
   late int pontosEquipe_2 = 0;
   late int fimJogo;
+
+  imprimeJogo() {
+    print(
+        "eq1: $equipe_1, eq2: $equipe_2, p1: $pontosEquipe_1, p2: $pontosEquipe_2, fim: $fimJogo");
+  }
 
   adicionaPontosEqp1(BuildContext context) {
     pontosEquipe_1++;
@@ -50,16 +57,15 @@ void _alertdialog(BuildContext context) {
       title: const Text("Fim de Jogo"),
       content: const Text('jogo encerado'),
       actions: <Widget>[
-        // FloatingActionButton(
-        //   child: const Text("fechar"),
-        //   onPressed: () {
-        //     Navigator.of(context).pop();
-        //   },
-        // ),
         FloatingActionButton(
           child: const Text("Novo Jogo"),
           onPressed: () {
-            Navigator.of(context).pushReplacementNamed('/');
+            Jogo jogo = Provider.of<Jogo>(context, listen: false);
+            Provider.of<ProviderJogo>(context, listen: false).createJogo(jogo);
+            print(
+                "Lista Jogo Inicio ${Provider.of<ProviderJogo>(context, listen: false).tamanhoListaJogos()}");
+            print("dados Jogo ${jogo.imprimeJogo()}");
+            Navigator.of(context).popAndPushNamed('/');
           },
         )
       ],
