90914ddec826522d3ee53c96e09bcaa796f0c944
MarcioBastos <marciobastosn@gmail.com>
2022-11-10 11:19:45 -0300

    alert erro app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:dia_de_sexta/model/jogo.dart';
 import 'package:dia_de_sexta/view/home.dart';
 import 'package:dia_de_sexta/view/placar.dart';
@@ -5,6 +7,31 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  ErrorWidget.builder = (FlutterErrorDetails details) {
+    bool inDebug = false;
+    assert(() {
+      inDebug = true;
+      return true;
+    }());
+
+    if (inDebug) {
+      return ErrorWidget(details.exception);
+    }
+
+    return Container(
+      alignment: Alignment.center,
+      child: Text(
+        "Error:\n ${details.exception}",
+        style: const TextStyle(
+          color: Colors.orange,
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  };
+
   runApp(const MyApp());
 }
 
