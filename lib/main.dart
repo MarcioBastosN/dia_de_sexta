dbb830ecf41c755661d731bbee5b1114432cf35c
Marcio Andre Neves Bastos <marciobastosn@gmail.com>
2022-11-14 12:08:38 -0300

    inicio sobre

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:dia_de_sexta/model/jogo.dart';
 import 'package:dia_de_sexta/view/home.dart';
 import 'package:dia_de_sexta/view/listaPlacar.dart';
 import 'package:dia_de_sexta/view/placar.dart';
+import 'package:dia_de_sexta/view/sobre.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -85,6 +86,7 @@ class MyApp extends StatelessWidget {
           '/': (context) => const Home(),
           'placar': (context) => const Placar(title: "Seu Placar vai à:"),
           'lista': (context) => const ListaPlacar(),
+          'sobre': (context) => const Sobre(),
         },
       ),
     );
---
+++ lib/view/sobre.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Sobre extends StatefulWidget {
+  const Sobre({super.key});
+
+  @override
+  State<Sobre> createState() => _SobreState();
+}
+
+class _SobreState extends State<Sobre> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaScreen = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              height: mediaScreen.height,
+              width: mediaScreen.width,
+              child: Row(
+                children: const [
+                  Expanded(
+                    child: Card(
+                      elevation: 5,
+                      child: Text("texto _1"),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Positioned(
+              top: 10,
+              right: 10,
+              child: SafeArea(
+                child: PopupMenuButton(
+                  color: Colors.cyan,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18),
+                  ),
+                  itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+                    PopupMenuItem(
+                      value: "Home",
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.of(context).pop();
+                          Navigator.of(context).popAndPushNamed('/');
+                        },
+                        child: Row(
+                          children: const [
+                            Icon(Icons.home),
+                            Text("Home"),
+                          ],
+                        ),
+                      ),
+                    ),
+                    PopupMenuItem(
+                      value: "Lista",
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.of(context).pop();
+                          Navigator.of(context).popAndPushNamed('lista');
+                        },
+                        child: Row(
+                          children: const [
+                            Icon(Icons.list),
+                            Text("Historico"),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
