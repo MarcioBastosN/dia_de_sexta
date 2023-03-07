0a2ebf953198e17d7ecdb64851c7b61d57f0a34f
Marcio Andre Neves Bastos <marciobastosn@gmail.com>
2023-03-07 16:47:55 -0300

    adicionado animacoes

--- lib/view/component/jogadores/grid_jogadores.dart
+++ lib/view/component/jogadores/grid_jogadores.dart
@@ -6,6 +6,7 @@ import 'package:provider/provider.dart';
 
 import '../dialog_component.dart';
 import '../text_form_compoment.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 class GridJogadores extends StatefulWidget {
   final FlipCardController? flip;
@@ -79,127 +80,144 @@ class _GridJogadoresState extends State<GridJogadores> {
 
     return Padding(
       padding: const EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 64.0),
-      child: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          childAspectRatio: 4 / 3.1,
-        ),
-        itemCount: listaJogadores.length,
-        itemBuilder: (context, index) {
-          return Scaffold(
-            backgroundColor: Theme.of(context).colorScheme.primary,
-            body: Card(
-              color: listaJogadores[index].id != null
-                  ? listaJogadores[index].possuiTime == 1
-                      ? Theme.of(context).colorScheme.outline
-                      : Theme.of(context).colorScheme.secondary
-                  : Theme.of(context).colorScheme.error,
-              child: DefaultTextStyle(
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.onPrimary,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-                child: SizedBox(
-                  height: 70,
-                  child: Center(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          top: 8, left: 16, right: 16, bottom: 16),
-                      child: Column(
-                        children: [
-                          Expanded(
-                            child: SingleChildScrollView(
-                              scrollDirection: Axis.vertical,
-                              child: Text(listaJogadores[index].nome!),
+      child: AnimationLimiter(
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            childAspectRatio: 4 / 3.1,
+          ),
+          itemCount: listaJogadores.length,
+          itemBuilder: (context, index) {
+            return Scaffold(
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              body: AnimationConfiguration.staggeredGrid(
+                duration: const Duration(milliseconds: 1000),
+                position: 0,
+                columnCount: 3,
+                child: FlipAnimation(
+                  child: Card(
+                    color: listaJogadores[index].id != null
+                        ? listaJogadores[index].possuiTime == 1
+                            ? Theme.of(context).colorScheme.outline
+                            : Theme.of(context).colorScheme.secondary
+                        : Theme.of(context).colorScheme.error,
+                    child: DefaultTextStyle(
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.onPrimary,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      child: SizedBox(
+                        height: 70,
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                top: 8, left: 16, right: 16, bottom: 16),
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: SingleChildScrollView(
+                                    scrollDirection: Axis.vertical,
+                                    child: Text(listaJogadores[index].nome!),
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                        ],
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-            floatingActionButtonLocation:
-                FloatingActionButtonLocation.centerDocked,
-            floatingActionButton: SpeedDial(
-              icon: Icons.menu,
-              mini: true,
-              overlayColor: Theme.of(context).colorScheme.primary,
-              backgroundColor: Theme.of(context).colorScheme.primary,
-              direction: SpeedDialDirection.down,
-              switchLabelPosition: verificaIndex(index),
-              children: [
-                SpeedDialChild(
-                  backgroundColor: Theme.of(context).colorScheme.secondary,
-                  labelBackgroundColor: Theme.of(context).colorScheme.secondary,
-                  labelStyle: TextStyle(
-                      color: Theme.of(context).colorScheme.onSecondary),
-                  label: "Editar",
-                  child: Icon(
-                    Icons.edit,
-                    color: Theme.of(context).colorScheme.onSecondary,
+              floatingActionButtonLocation:
+                  FloatingActionButtonLocation.centerDocked,
+              floatingActionButton: SpeedDial(
+                icon: Icons.menu,
+                mini: true,
+                overlayColor: Theme.of(context).colorScheme.primary,
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                direction: SpeedDialDirection.down,
+                switchLabelPosition: verificaIndex(index),
+                children: [
+                  SpeedDialChild(
+                    backgroundColor: Theme.of(context).colorScheme.secondary,
+                    labelBackgroundColor:
+                        Theme.of(context).colorScheme.secondary,
+                    labelStyle: TextStyle(
+                        color: Theme.of(context).colorScheme.onSecondary),
+                    label: "Editar",
+                    child: Icon(
+                      Icons.edit,
+                      color: Theme.of(context).colorScheme.onSecondary,
+                    ),
+                    onTap: () {
+                      updateJogadorLista(context, listaJogadores[index]);
+                    },
                   ),
-                  onTap: () {
-                    updateJogadorLista(context, listaJogadores[index]);
-                  },
-                ),
-                SpeedDialChild(
-                  backgroundColor: Theme.of(context).colorScheme.secondary,
-                  labelBackgroundColor: Theme.of(context).colorScheme.secondary,
-                  labelStyle: TextStyle(
-                      color: Theme.of(context).colorScheme.onSecondary),
-                  label: "Apagar",
-                  child: Icon(
-                    Icons.delete,
-                    color: Theme.of(context).colorScheme.onSecondary,
+                  SpeedDialChild(
+                    backgroundColor: Theme.of(context).colorScheme.secondary,
+                    labelBackgroundColor:
+                        Theme.of(context).colorScheme.secondary,
+                    labelStyle: TextStyle(
+                        color: Theme.of(context).colorScheme.onSecondary),
+                    label: "Apagar",
+                    child: Icon(
+                      Icons.delete,
+                      color: Theme.of(context).colorScheme.onSecondary,
+                    ),
+                    visible:
+                        listaJogadores[index].possuiTime != 1 ? true : false,
+                    onTap: () {
+                      if (listaJogadores[index].possuiTime != 1) {
+                        Provider.of<Jogador>(context, listen: false)
+                            .removeJogador(listaJogadores[index]);
+                      }
+                    },
                   ),
-                  visible: listaJogadores[index].possuiTime != 1 ? true : false,
-                  onTap: () {
-                    if (listaJogadores[index].possuiTime != 1) {
-                      Provider.of<Jogador>(context, listen: false)
-                          .removeJogador(listaJogadores[index]);
-                    }
-                  },
-                ),
-                SpeedDialChild(
-                  backgroundColor: Theme.of(context).colorScheme.secondary,
-                  labelBackgroundColor: Theme.of(context).colorScheme.secondary,
-                  labelStyle: TextStyle(
-                      color: Theme.of(context).colorScheme.onSecondary),
-                  label: "liberar",
-                  child: Icon(
-                    Icons.refresh,
-                    color: Theme.of(context).colorScheme.onSecondary,
+                  SpeedDialChild(
+                    backgroundColor: Theme.of(context).colorScheme.secondary,
+                    labelBackgroundColor:
+                        Theme.of(context).colorScheme.secondary,
+                    labelStyle: TextStyle(
+                        color: Theme.of(context).colorScheme.onSecondary),
+                    label: "liberar",
+                    child: Icon(
+                      Icons.refresh,
+                      color: Theme.of(context).colorScheme.onSecondary,
+                    ),
+                    visible:
+                        listaJogadores[index].possuiTime == 1 ? true : false,
+                    onTap: () {
+                      // libera o jogador e remove do grupo
+                      if (listaJogadores[index].possuiTime == 1) {
+                        Provider.of<Jogador>(context, listen: false)
+                            .liberaJogadorId(
+                                listaJogadores[index].id!, context);
+                      }
+                    },
                   ),
-                  visible: listaJogadores[index].possuiTime == 1 ? true : false,
-                  onTap: () {
-                    // libera o jogador e remove do grupo
-                    if (listaJogadores[index].possuiTime == 1) {
-                      Provider.of<Jogador>(context, listen: false)
-                          .liberaJogadorId(listaJogadores[index].id!, context);
-                    }
-                  },
-                ),
-                SpeedDialChild(
-                  backgroundColor: Theme.of(context).colorScheme.secondary,
-                  labelBackgroundColor: Theme.of(context).colorScheme.secondary,
-                  labelStyle: TextStyle(
-                      color: Theme.of(context).colorScheme.onSecondary),
-                  label: "adicionar a um time",
-                  // child: const Icon(Icons.refresh),
-                  visible: listaJogadores[index].possuiTime != 1 ? true : false,
-                  onTap: () {
-                    widget.flip!.flipcard();
-                  },
-                ),
-              ],
-            ),
-          );
-        },
+                  SpeedDialChild(
+                    backgroundColor: Theme.of(context).colorScheme.secondary,
+                    labelBackgroundColor:
+                        Theme.of(context).colorScheme.secondary,
+                    labelStyle: TextStyle(
+                        color: Theme.of(context).colorScheme.onSecondary),
+                    label: "adicionar a um time",
+                    // child: const Icon(Icons.refresh),
+                    visible:
+                        listaJogadores[index].possuiTime != 1 ? true : false,
+                    onTap: () {
+                      widget.flip!.flipcard();
+                    },
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
--- lib/view/view_lista_placar.dart
+++ lib/view/view_lista_placar.dart
@@ -12,7 +12,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:screenshot/screenshot.dart';
 import 'package:social_share/social_share.dart';
-
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'component/alert_exit.dart';
 
 class ListaPlacar extends StatefulWidget {
@@ -148,55 +148,67 @@ class _MyWidgetState extends State<ListaPlacar> {
         backgroundColor: Theme.of(context).colorScheme.primary,
         appBar: appBar,
         body: Provider.of<Jogo>(context, listen: false).tamanhoListaJogos() > 0
-            ? ListView.builder(
-                reverse: true,
-                addRepaintBoundaries: true,
-                scrollDirection: Axis.vertical,
-                padding: const EdgeInsets.all(8.0),
-                itemCount: listaJogo.length,
-                itemBuilder: (context, int index) {
-                  return Slidable(
-                    key: ValueKey(listaJogo[index].id),
-                    startActionPane: ActionPane(
-                      motion: const StretchMotion(),
-                      children: [
-                        SlidableAction(
-                          onPressed: (context) {
-                            Provider.of<Jogo>(context, listen: false)
-                                .removeJogo(listaJogo[index]);
-                          },
-                          backgroundColor: Theme.of(context).colorScheme.error,
-                          foregroundColor:
-                              Theme.of(context).colorScheme.onError,
-                          icon: Icons.delete,
-                          label: 'Apagar',
+            ? AnimationLimiter(
+                child: ListView.builder(
+                    reverse: true,
+                    addRepaintBoundaries: true,
+                    scrollDirection: Axis.vertical,
+                    padding: const EdgeInsets.all(8.0),
+                    itemCount: listaJogo.length,
+                    itemBuilder: (context, int index) {
+                      return Slidable(
+                        key: ValueKey(listaJogo[index].id),
+                        startActionPane: ActionPane(
+                          motion: const StretchMotion(),
+                          children: [
+                            SlidableAction(
+                              onPressed: (context) {
+                                Provider.of<Jogo>(context, listen: false)
+                                    .removeJogo(listaJogo[index]);
+                              },
+                              backgroundColor:
+                                  Theme.of(context).colorScheme.error,
+                              foregroundColor:
+                                  Theme.of(context).colorScheme.onError,
+                              icon: Icons.delete,
+                              label: 'Apagar',
+                            ),
+                            SlidableAction(
+                              onPressed: (context) {
+                                compartilhar(context, listaJogo[index]);
+                              },
+                              backgroundColor:
+                                  Theme.of(context).colorScheme.secondary,
+                              foregroundColor:
+                                  Theme.of(context).colorScheme.onSecondary,
+                              icon: Icons.share,
+                              label: 'Compartilhar',
+                            ),
+                          ],
                         ),
-                        SlidableAction(
-                          onPressed: (context) {
-                            compartilhar(context, listaJogo[index]);
-                          },
-                          backgroundColor:
-                              Theme.of(context).colorScheme.secondary,
-                          foregroundColor:
-                              Theme.of(context).colorScheme.onSecondary,
-                          icon: Icons.share,
-                          label: 'Compartilhar',
+                        child: AnimationConfiguration.staggeredList(
+                          duration: const Duration(milliseconds: 1000),
+                          delay: const Duration(milliseconds: 10),
+                          position: 0,
+                          child: FlipAnimation(
+                            child: CardListaPlacar(
+                              indexCard: (index + 1).toString(),
+                              equipe1: Provider.of<Time>(context, listen: false)
+                                  .retornaNomeTime(listaJogo[index].equipe_1!),
+                              equipe2: Provider.of<Time>(context, listen: false)
+                                  .retornaNomeTime(listaJogo[index].equipe_2!),
+                              pontosEq1:
+                                  listaJogo[index].pontosEquipe_1.toString(),
+                              pontosEq2:
+                                  listaJogo[index].pontosEquipe_2.toString(),
+                              data: listaJogo[index].data.toString(),
+                              tempo: listaJogo[index].tempoJogo!,
+                            ),
+                          ),
                         ),
-                      ],
-                    ),
-                    child: CardListaPlacar(
-                      indexCard: (index + 1).toString(),
-                      equipe1: Provider.of<Time>(context, listen: false)
-                          .retornaNomeTime(listaJogo[index].equipe_1!),
-                      equipe2: Provider.of<Time>(context, listen: false)
-                          .retornaNomeTime(listaJogo[index].equipe_2!),
-                      pontosEq1: listaJogo[index].pontosEquipe_1.toString(),
-                      pontosEq2: listaJogo[index].pontosEquipe_2.toString(),
-                      data: listaJogo[index].data.toString(),
-                      tempo: listaJogo[index].tempoJogo!,
-                    ),
-                  );
-                })
+                      );
+                    }),
+              )
             : Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
